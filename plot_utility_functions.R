af749dbeeb540d24ffa766fed6fb7e7cb4881a11
nassuphis <nassuphis@users.noreply.github.com>
2017-11-17 14:26:15 +0000

    Update plot_utility_functions.R

--- plot_utility_functions.R
+++ plot_utility_functions.R
@@ -1,4 +1,34 @@
 
+# map XY coordinates to fixed grid points
+# for plotting and table layouts
+force2grid<-function(
+  df,
+  col_count=ceiling(sqrt(nrow(df)))+col_slack,
+  row_count=ceiling(nrow(df)/col_count)+row_slack,
+  col_slack=1,
+  row_slack=1,
+  table_target=FALSE
+){
+  m<-matrix("",nrow=row_count,ncol=col_count)
+  gx<-as.vector(col(m))
+  gy<-as.vector(row(m))
+  dfx<-rescale(df$x,range(gx)) #bin(df$x,max(gx))
+  dfy<-rescale(df$y,range(gy)) #bin(df$y,max(gy))
+  mx<-matrix(dfx,ncol=1)[,rep(1,length(gx)),drop=FALSE]
+  my<-matrix(dfy,ncol=1)[,rep(1,length(gx)),drop=FALSE]
+  gridx<-matrix(gx,nrow=1)[rep(1,length(dfx)),,drop=FALSE]
+  gridy<-matrix(gy,nrow=1)[rep(1,length(dfy)),,drop=FALSE]
+  dx<-(mx-gridx)^2
+  dy<-(my-gridy)^2
+  d<-dx+dy
+  t2g <- solve_LSAP(d)
+  data.table(
+    x=gx[t2g],
+    y=gy[t2g]
+  )
+}
+
+
 grid_plot<-function(data,show_grid=TRUE){
   n<-length(data)
   xw<-rep(1,n)
