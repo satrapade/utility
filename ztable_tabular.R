0bcee9f04ccd71968520e0174a8b9928090fd864
nassuphis <nassuphis@users.noreply.github.com>
2018-05-25 10:52:23 +0100

    Update ztable_tabular.R

--- ztable_tabular.R
+++ ztable_tabular.R
@@ -1,17 +1,17 @@
 
 require(ztable)
 
-ztabular<-function(x){
+ztabular<-function(x,colors=TRUE,center=TRUE){
   if(class(x)!="ztable")return("")
   x0<-capture.output(print(x,type="latex"))
   start<-head(which(grepl("(^\\\\begin\\{tabular\\})|(^\\\\begin\\{longtable\\})",x0),1))
   end<-head(which(grepl("(^\\\\end\\{tabular\\})|(^\\\\end\\{longtable\\})",x0),1))
   cols<-which(grepl("^\\\\definecolor\\{[a-z0-9\\-]+\\}",x0),1)
   x1<-paste0(c(
-    "\\begin{center}\n",
-    x0[cols],
+    if(colors)x0[cols]else"",
+    if(center)"\\begin{center}\n"else"",
     x0[start:end],
-    "\\end{center}\n"
+    if(center)"\\end{center}\n"else""
   ),collapse="\n")
   x1
 }
