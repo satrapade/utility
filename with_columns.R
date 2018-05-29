02c7fc614dd849d4fa7214978fe4dd8da32242ee
nassuphis <nassuphis@users.noreply.github.com>
2018-05-29 11:39:57 +0100

    Create with_columns.R

---
+++ with_columns.R
@@ -0,0 +1,25 @@
+
+# add arguments with default values to function formals
+with_formals<-function(fun,form){
+  for(i in setdiff(names(form),"")){
+    formals(fun)[[i]]<-form[[i]]
+  }
+  fun
+}
+
+# perform calculation on matrix columns
+with_columns<-function(expr,...,MoreArgs=NULL){
+  mat<-list(...)
+  n<-unique(mapply(ncol,mat))
+  if(length(n)>1)stop("unequal column counts")
+  df<-lapply(mat,data.table)
+  the_fun<-with_formals(function(){},c(structure(rep(list(numeric(0)),length(mat)),.Names=names(mat)),MoreArgs))
+  body(the_fun)<-expr
+  args<-c(list(FUN=the_fun,SIMPLIFY=FALSE,MoreArgs=MoreArgs),df)
+  res0<-do.call(mapply,args)
+  res1<-do.call(cbind,res0)
+  rownames(res1)<-rownames(mat[[1]])
+  res1
+}
+
+
