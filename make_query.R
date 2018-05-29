91fc2927477c8e74b037dbb5c03fe747449fd0a2
nassuphis <nassuphis@users.noreply.github.com>
2018-05-29 14:00:25 +0100

    Update make_query.R

--- make_query.R
+++ make_query.R
@@ -1,6 +1,20 @@
 require(gsubfn)
 require(stringi)
 
+query<-function(statement,db=get("db",parent.frame())){
+  q<-try(dbSendQuery(conn=db,statement),silent = TRUE)
+  if(any(class(q)=="try-error")){
+    cat(as.character(attributes(q)$condition))
+    stop(attributes(q)$condition)
+  }
+  r<-try(dbFetch(q,n=-1),silent=TRUE)
+  if(any(class(r)=="try-error")){
+    cat(as.character(attributes(r)$condition))
+    stop(attributes(r)$condition)
+  }
+  dbClearResult(q)
+  data.table(r)
+}
 
 # 
 # file include and macro expansion
