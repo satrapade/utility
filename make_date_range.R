ffbecd3406bac1b4b6697b883ab8b8bdd835bbee
nassuphis <nassuphis@users.noreply.github.com>
2018-06-04 15:12:02 +0100

    Update make_date_range.R

--- make_date_range.R
+++ make_date_range.R
@@ -2,11 +2,12 @@
 
 make_date_range<-function(start, end)
 {
-  from <- as.Date(start, format = "%Y-%m-%d")
-  to <- as.Date(end, format = "%Y-%m-%d")
+  from <- as.Date(gsub("[\"\'a-z]","",start), format = "%Y-%m-%d")
+  to <- as.Date(gsub("[\"\'a-z]","",end), format = "%Y-%m-%d")
   date_seq <- seq(from = from, to = to, by = 1)
   res  <- as.character(date_seq, format = "%Y-%m-%d" )
   return(res)
 }
+
 
 
