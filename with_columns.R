520b671e8de3d8423022108bb597fee8ef66e7a1
nassuphis <nassuphis@users.noreply.github.com>
2018-06-06 16:30:43 +0100

    Update with_columns.R

--- with_columns.R
+++ with_columns.R
@@ -7,27 +7,18 @@
 # }),mv=pair_mv,tret=pair_tret)
 #
 
-# add arguments with default values to function formals
-with_formals<-function(fun,form){
-  for(i in setdiff(names(form),"")){
-    formals(fun)[[i]]<-form[[i]]
-  }
-  fun
-}
-
-# perform calculation on matrix columns
-with_columns<-function(expr,...,MoreArgs=NULL){
+with_columns<-function(expr,...,MoreArgs=list()){
   mat<-list(...)
-  n<-unique(mapply(ncol,mat))
-  if(length(n)>1)stop("unequal column counts")
-  df<-lapply(mat,data.table)
-  the_fun<-with_formals(function(){},c(structure(rep(list(numeric(0)),length(mat)),.Names=names(mat)),MoreArgs))
-  body(the_fun)<-expr
-  args<-c(list(FUN=the_fun,SIMPLIFY=FALSE,MoreArgs=MoreArgs),df)
-  res0<-do.call(mapply,args)
-  res1<-do.call(cbind,res0)
-  rownames(res1)<-rownames(mat[[1]])
-  res1
+  if(length(mat)<1)stop("no inputs")
+  if(length(unique(mapply(ncol,mat)))>1)stop("unequal column counts")
+  if(is.null(MoreArgs$ndx))MoreArgs$ndx<-1:ncol(mat[[1]])
+  res<-matrix(0,nrow=nrow(mat[[1]]),ncol=length(MoreArgs$ndx),dimnames=list(NULL,NULL))
+  the_expressions<-setNames(lapply(seq_along(mat),function(v)bquote(mat[[.(v)]][,MoreArgs$ndx[i] ])),names(mat))
+  the_environment<-c(the_expressions,MoreArgs)
+  e1<-as.expression(do.call(substitute,list(expr[[1]],the_environment)))
+  for(i in 1:length(MoreArgs$ndx))res[,i]<-eval(e1)
+  dimnames(res)<-dimnames(mat[[1]][,MoreArgs$ndx])
+  res
 }
 
 stopifnot(all(with_columns(expr=expression(x+y),x=matrix(1:100,ncol=10),y=diag(10))==matrix(1:100,ncol=10)+diag(10)))
