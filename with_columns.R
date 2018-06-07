60480fafec2a66e9cb3d870c298c0f1e347f68b1
nassuphis <nassuphis@users.noreply.github.com>
2018-06-07 13:01:07 +0100

    Update with_columns.R

--- with_columns.R
+++ with_columns.R
@@ -10,7 +10,10 @@
 with_columns<-function(expr,...,MoreArgs=list()){
   mat<-list(...)
   if(length(mat)<1)stop("no inputs")
-  if(length(unique(mapply(ncol,mat)))>1)stop("unequal column counts")
+  if(length(unique(mapply(ncol,mat)))>1){
+    print(mapply(ncol,mat))
+    stop("unequal column counts")
+   }
   if(is.null(MoreArgs$ndx))MoreArgs$ndx<-1:ncol(mat[[1]])
   res<-matrix(0,nrow=nrow(mat[[1]]),ncol=length(MoreArgs$ndx),dimnames=list(NULL,NULL))
   the_expressions<-setNames(lapply(seq_along(mat),function(v)bquote(mat[[.(v)]][,MoreArgs$ndx[i] ])),names(mat))
